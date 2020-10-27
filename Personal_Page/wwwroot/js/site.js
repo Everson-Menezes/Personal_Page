1ee05e860df1410a6f0160019b6b9184dccad572
Everson dos Santos de Menezes <everson-menezes@bol.com.br>
2020-10-27 17:31:05 -0300

    atualização

--- Personal_Page/wwwroot/js/site.js
+++ Personal_Page/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+<script>
+    function teste() {
+        alert("Passou o mouse")
+    }
+</script>
